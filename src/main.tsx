a15ce923ae046f96f6525143c4ab469d72a1653d
Gokul-96 <gokulakrishnan.m96@gmail.com>
2024-09-07 19:42:41 +0530

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Tailwind CSS import
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Find the root element in the HTML file where the app will be mounted
+const rootElement = document.getElementById('root') as HTMLElement;
+
+const root = ReactDOM.createRoot(rootElement);
+
+// Render the App component inside the root element
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
